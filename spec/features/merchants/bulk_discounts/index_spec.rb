25f6be12cb608e80fa1896684df97c10b3988c5c
George Ho <george.h.w.ho@gmail.com>
2021-04-26 13:01:52 -0400

    Add more robust tests to bulk discount index page

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -4,6 +4,7 @@ describe 'bulk discount index page' do
   before {
     @merchant = create(:random_merchant)
     @bd_1 = create(:random_bulk_discount, merchant: @merchant)
+    @bd_2 = create(:random_bulk_discount, merchant: @merchant)
 
     visit merchant_dashboard_index_path(@merchant)
     click_on("My Discounts")
@@ -16,6 +17,10 @@ describe 'bulk discount index page' do
       expect(page).to have_content(@bd_1.name)
       expect(page).to have_content(@bd_1.threshold)
       expect(page).to have_content(@bd_1.percent_discount)
+      expect(page).to have_link(@bd_2.id)
+      expect(page).to have_content(@bd_2.name)
+      expect(page).to have_content(@bd_2.threshold)
+      expect(page).to have_content(@bd_2.percent_discount)
     end
   end
 end
