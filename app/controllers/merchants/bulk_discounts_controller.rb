918fba3c43fd91c0eeea6cb8558c13916153323f
George Ho <george.h.w.ho@gmail.com>
2021-04-26 17:14:21 -0400

    Add bulk discount edit

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -24,6 +24,21 @@ class Merchants::BulkDiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+    if bulk_discount.update(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(merchant, bulk_discount)
+    else
+      redirect_to edit_merchant_bulk_discount_path(merchant, bulk_discount)
+      flash[:error] = "Error: #{error_message(bulk_discount.errors)}"
+    end
+  end
+
   def destroy
     BulkDiscount.find(params[:id]).destroy
     redirect_to merchant_bulk_discounts_path
--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -11,12 +11,18 @@ describe 'bulk discount show page' do
   }
 
   context 'you click on first bulk discount' do
-    it 'shows you the name, threshold, and percent discount' do
+    before {
       click_on(@bd_1.id)
+    }
+    it 'shows you the name, threshold, and percent discount' do
       expect(page).to have_content(@bd_1.name)
       expect(page).to have_content(@bd_1.threshold)
       expect(page).to have_content(@bd_1.percent_discount)
       expect(current_path).to eq(merchant_bulk_discount_path(@merchant, @bd_1))
+    end
+
+    it 'shows you a edit link' do
+      expect(page).to have_link("Edit Bulk Discount")
     end
   end
 end
